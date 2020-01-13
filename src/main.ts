6d92c8a89d694d21f2cc681a5a4df3132269fe8a
Chad Ostrowski <hi@chadoh.com>
2020-01-13 10:31:19 -0500

    Walk Blocks: add placeholder block-walking logic
    We want to find all _failed_ DAO creations, in addition to successful
    DAO creations.
    
    As far as our research has determined, the only way to do this is to
    literally walk every single block from the creation block of our
    contracts, checking transactions for ones against our contract.
    
    😟 😨 😱
    
    400,000 blocks to scan through. NBD.
    
    This does not work, probably because of Infura rate limitations.
    
    Where do we go from here?
    
    It's probably time to try out The Graph, to see if we can aggregate
    these stats with their backend, so this frontend can just grab the info
    it needs over GraphQL.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import getSuccesses from './getSuccesses'
+import getSuccesses, { walkBlocks } from './getSuccesses'
 
 Array.prototype.toString = function() {
   return this.join('')
@@ -8,50 +8,52 @@ async function main() {
   const data = document.getElementById('data')
   if (!data) return
 
-  const daosCreated = await getSuccesses()
-  data.innerHTML = `
-    <h1>
-      ${daosCreated.length} Open Enterprise DAOs have been created on mainnet
-    </h1>
-    <table>
-      <thead>
-        <tr>
-          <th>&nbsp;</th>
-          <th>address</th>
-          <th>blockHash</th>
-          <th>blockNumber</th>
-          <th>logIndex</th>
-          <th>transactionHash</th>
-          <th>transactionIndex</th>
-          <th>returnValues.dao</th>
-          <th>event</th>
-          <th>signature</th>
-          <th>raw.data</th>
-        </tr>
-      </thead>
-      <tbody>
-        ${daosCreated.map((dao, index) => `
-          <tr>
-            <td>${index + 1}</td>
-            <td><code>${dao.address}</code></td>
-            <td><code>${dao.blockHash}</code></td>
-            <td><code>${dao.blockNumber}</code></td>
-            <td><code>${dao.logIndex}</code></td>
-            <td><code>${dao.transactionHash}</code></td>
-            <td><code>${dao.transactionIndex}</code></td>
-            <td>
-              <a target="_blank" href="https://mainnet.aragon.org/#/${dao.returnValues.dao}">
-                <code>${dao.returnValues.dao}</code>
-              </a>
-            </td>
-            <td><code>${dao.event}</code></td>
-            <td><code>${dao.signature}</code></td>
-            <td><code>${dao.raw.data}</code></td>
-          </tr>
-        `)}
-      </tbody>
-    </table>
-  `
+  // const daosCreated = await getSuccesses()
+  // data.innerHTML = `
+  //   <h1>
+  //     ${daosCreated.length} Open Enterprise DAOs have been created on mainnet
+  //   </h1>
+  //   <table>
+  //     <thead>
+  //       <tr>
+  //         <th>&nbsp;</th>
+  //         <th>address</th>
+  //         <th>blockHash</th>
+  //         <th>blockNumber</th>
+  //         <th>logIndex</th>
+  //         <th>transactionHash</th>
+  //         <th>transactionIndex</th>
+  //         <th>returnValues.dao</th>
+  //         <th>event</th>
+  //         <th>signature</th>
+  //         <th>raw.data</th>
+  //       </tr>
+  //     </thead>
+  //     <tbody>
+  //       ${daosCreated.map((dao, index) => `
+  //         <tr>
+  //           <td>${index + 1}</td>
+  //           <td><code>${dao.address}</code></td>
+  //           <td><code>${dao.blockHash}</code></td>
+  //           <td><code>${dao.blockNumber}</code></td>
+  //           <td><code>${dao.logIndex}</code></td>
+  //           <td><code>${dao.transactionHash}</code></td>
+  //           <td><code>${dao.transactionIndex}</code></td>
+  //           <td>
+  //             <a target="_blank" href="https://mainnet.aragon.org/#/${dao.returnValues.dao}">
+  //               <code>${dao.returnValues.dao}</code>
+  //             </a>
+  //           </td>
+  //           <td><code>${dao.event}</code></td>
+  //           <td><code>${dao.signature}</code></td>
+  //           <td><code>${dao.raw.data}</code></td>
+  //         </tr>
+  //       `)}
+  //     </tbody>
+  //   </table>
+  // `
+
+  walkBlocks()
 }
 
 document.body.onload = main
